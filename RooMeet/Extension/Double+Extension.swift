f534b7da3d710d4a192d892739903884491279e9
kgcoc0114 <kgcoc0114@gmail.com>
2022-12-05 23:53:56 +0800

    fix: setup setting

---
+++ RooMeet/Extension/Double+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Double+Extension.swift
+//  RooMeet
+//
+//  Created by kgcoc on 2022/12/5.
+//
+
+import Foundation
---
+++ RooMeet/Model/AddRoomPost/PostSection.swift
@@ -0,0 +1,8 @@
+//
+//  PostSection.swift
+//  RooMeet
+//
+//  Created by kgcoc on 2022/12/5.
+//
+
+import Foundation
